72cce03584b586786ddaca19ea3884aa0a188c53
Maxime Juncker <maxime.junck@gmail.com>
2024-11-09 14:46:38 +0100

    [fix lstmap]: new was free in case of wrong malloc

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mjuncker <mjuncker@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 16:11:12 by mjuncker          #+#    #+#             */
-/*   Updated: 2024/11/08 09:42:27 by mjuncker         ###   ########.fr       */
+/*   Updated: 2024/11/09 14:44:27 by mjuncker         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,12 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*new;
 
 	map = NULL;
-	if (lst == NULL || f == NULL || del == NULL)
-		return (NULL);
 	while (lst)
 	{
 		new = ft_lstnew(f(lst->content));
 		if (new == NULL)
 		{
 			ft_lstclear(&map, del);
-			ft_lstdelone(new, del);
 			return (NULL);
 		}
 		ft_lstadd_back(&map, new);
