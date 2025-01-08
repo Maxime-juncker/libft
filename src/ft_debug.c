fc63962ea41d7e1e88445d305ba7669b133bf98a
Maxime Juncker <maxime.junck@gmail.com>
2025-01-08 15:22:40 +0100

    fix alignement issue in ft_debug.c

--- src/ft_debug.c
+++ src/ft_debug.c
@@ -6,7 +6,7 @@
 /*   By: mjuncker <mjuncker@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 13:51:52 by mjuncker          #+#    #+#             */
-/*   Updated: 2025/01/08 15:19:26 by mjuncker         ###   ########.fr       */
+/*   Updated: 2025/01/08 15:22:20 by mjuncker         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 // colors
 #define RESET	"\033[0m"
 #define BLACK	"\033[0;30m"
-#define RED	"\033[0;31m"
+#define RED		"\033[0;31m"
 #define GREEN	"\033[0;32m"
 #define YELLOW	"\033[0;33m"
 #define BLUE	"\033[0;34m"
@@ -25,14 +25,14 @@
 #define GRAY	"\033[0;30m"
 
 // Background
-#define B_BLACK	"\033[40m"
+#define B_BLACK		"\033[40m"
 #define B_RED		"\033[41m"
-#define B_GREEN	"\033[42m"
+#define B_GREEN		"\033[42m"
 #define B_YELLOW	"\033[43m"
 #define B_BLUE		"\033[44m"
 #define B_PURPLE	"\033[45m"
 #define B_CYAN		"\033[46m"
-#define B_WHITE	"\033[47m"
+#define B_WHITE		"\033[47m"
 
 void	debug_color(char *msg, char *color)
 {
